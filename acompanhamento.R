df9374968f5a8ffb49151e68f36c184e61f4022d
Mateus Seixas <mateus_seixas@hotmail.com.br>
2022-05-13 09:49:44 -0300

    cleanning

--- acompanhamento.R
+++ acompanhamento.R
@@ -1,62 +1,118 @@
-library(config)
+library(config, warn.conflicts = FALSE)
 library(ggplot2)
 library(tikzDevice)
 
-config <- config::get()
+# Generate a .pdf of the graph
+pdf(file="./output/Rplots.pdf")
 
-meses <- config$meses
+# Search the most recent config file
+files = list.files("./")
+max = 0
+for (i in 1 : length(files) ) {
+  # Collect the files
+  strg = files[i]
+  # Get the first three parts of the file
+  config_part = substr(strg, start = 1, stop = 6)
+  # Check the file with config name
+  if (config_part == "config" & strg != "config.yml") {
+    # Get the number part of the file
+    numberpart = regmatches(strg, gregexpr("[[:digit:]]+", strg))
+    number = as.numeric(unlist(numberpart))
+    # Get the max number of the config files
+    if (number > max) {
+      max = number
+    }
+  }
+}
+
+# Select config file based on max value
+if (max == 0) {
+  str_base = "config.yml"
+  config <- config::get(file = str_base)
+} else {
+  config <- config::get(file = sprintf("config%s.yml", max))
+}
+
+# Get data
 planejado <- config$planejado
-replanejado <- config$replanejado
 realizado <- config$realizado
-estimado <- config$estimado
+esforcos <- config$esforcos
 datas <- config$datas
+name <- config$name
 
+# Get vectors lenght
 l.planejado <- length(planejado)
-l.replanejado <- length(replanejado)
 l.realizado <- length(realizado)
-l.estimado <- length(estimado)
+l.esforcos <- length(esforcos)
+l.datas <- length(datas)
 
-d <- l.planejado + l.replanejado - meses - 1
-d
-plan <- c(planejado,replanejado[(1 + d) : l.replanejado])
-d2 <- l.realizado + l.estimado - meses - 1
-exec <- c(realizado,estimado[(1 + d2) : l.estimado])
-eficiencias <- exec * 100 / plan
-eficiencia.realizada <- eficiencias[2:l.realizado]
-eficiencia.estimada <- eficiencias[(l.realizado + 1):(meses+1)]
-join <- c(eficiencias,planejado,replanejado,realizado,estimado)
-max.join <- max(join[2:length(join)])
+# Generate error config messages
+if (sum(esforcos) != 1) {
+  cat("MENSAGEM DE ERRO: A SOMA DO VETOR DE esforcos DEVE SER IGUAL A 1")
+  quit()
+}
+if (l.realizado + l.esforcos != l.planejado) {
+  cat("MENSAGEM DE ERRO: A SOMA DO TAMANHO VETOR realizado COM O VETOR esforcos DEVE SER IGUAL AO TAMANHO DO
+      VETOR planejado. RECONFIGURE O ARQUIVO config.yml")
+  quit()
+}
+if (l.planejado != l.datas) {
+  cat("MENSAGEM DE ERRO: O TAMANHO DO VETOR datas TEM QUE SER IGUAL AO TAMNHO DO VETOR planejado. RECONFIGURE O ARQUIVO config.yml")
+  quit()
+}
 
+# Construct estimated vector
+concluido <- tail(realizado, n = 1)
+falta <- 100 - concluido
+estimado <- esforcos * falta
+estimado <- cumsum(estimado)
+estimado <- estimado + concluido
+
+# Construct x axes
 x.planejado <- 0 : (l.planejado - 1)
-x.replanejado <- (meses - l.replanejado + 1) : meses
 x.realizado <- 0 : (l.realizado - 1)
-x.estimado <- (meses - l.estimado + 1)  : meses
+x.estimado <- (l.realizado - 1) : (l.planejado - 1)
+x.estimado2 <- (l.realizado) : (l.planejado - 1)
 
+# Calculate efficiency  
+eficiencia.realizada <- realizado[2 : l.realizado] * 100/ planejado[2 : (l.realizado)]
+eficiencia.estimada <- estimado * 100/ planejado[(l.realizado + 1) :l.planejado]
+
+# Construct efficiency axis
+x.eficiencia.realizada <- 1 : (l.realizado - 1)
+
+# Construct estimated vector for plotting
+estimado <- c(concluido,estimado)
+
+# Construct data frames
 df.planejado <- data.frame(x = x.planejado, y = planejado)
-df.replanejado <- data.frame(x = x.replanejado, y = replanejado)
 df.realizado <- data.frame(x = x.realizado, y = realizado)
 df.estimado <- data.frame(x = x.estimado, y = estimado)
-df.eficiencia.realizada <- data.frame(x = 1:(l.realizado-1) , y = eficiencia.realizada)
-df.eficiencia.estimada <- data.frame(x = l.realizado:meses , y = eficiencia.estimada)
+df.eficiencia.realizada <- data.frame(x = x.eficiencia.realizada , y = eficiencia.realizada)
+df.eficiencia.estimada <- data.frame(x = x.estimado2 , y = eficiencia.estimada)
 
-colors <- c("Planejado" = "blue", "Replanejado" = "dodgerblue4", "Realizado" = "green","Estimado" = "seagreen4")
-fills <- c("Eficiencia Realizada"="gray75","Eficiencia Estimada"="gray40")
+# Declaration plot colors
+colors <- c("Planejado" = "blue", "Realizado" = "green","Estimado" = "seagreen4")
+fills <- c("Eficiencia Realizada"="gray40","Eficiencia Estimada"="gray75")
 
+# Generate plot
 a <- ggplot() +
   geom_bar(data = df.eficiencia.realizada, aes(x = x, y = y, fill="Eficiencia Realizada"),stat="identity") +
   geom_bar(data = df.eficiencia.estimada, aes(x = x, y = y, fill="Eficiencia Estimada"),stat="identity") +
   geom_line(data = df.estimado, aes(x = x, y = y, color = "Estimado"),stat="identity", size = 1.5) +
   geom_line(data = df.realizado, aes(x = x, y = y, color = "Realizado"),stat="identity", size = 1.5) +
   geom_line(data = df.planejado, aes(x = x, y = y, color = "Planejado"),stat="identity", size = 1.5) +
-  geom_line(data = df.replanejado, aes(x = x, y = y, color = "Replanejado"),stat="identity", size = 1.5)
-
-a <- a + xlab("Data") + ylab("Porcentagem") + labs(title="Acompanhamento do Projeto") + labs(color="Legenda") +
-  scale_color_manual(values = colors) + labs(fill = "Eficiencia") + scale_fill_manual(values = fills) +
-  scale_x_continuous(labels = datas, breaks = 0:meses) + scale_y_continuous(breaks = seq(0,max.join+10, by=10)) +
-  theme(plot.title = element_text(hjust = 0.5)) + theme(plot.title = element_text(size=18))
+    xlab("Data") + ylab("Porcentagem") + labs(title=name) + labs(color="Legenda") +
+  scale_color_manual(values = colors) + theme(plot.title = element_text(size=18)) +
+  theme(plot.title = element_text(hjust = 0.5)) + labs(fill = "Eficiencia") + scale_fill_manual(values = fills) +
+  scale_x_continuous(labels = datas, breaks = 0:(l.planejado - 1)) 
 a
+#todo eu sugiro inserir o nome do projeto no título do gráfico
 
+# Save image plot (PNG)
 ggsave("./output/graph.png", width = 10, height = 10)
+# Save LaTeX code
 tikz('./output/graph.tex', width = 10, height = 10)
 a
 dev.off()
+
